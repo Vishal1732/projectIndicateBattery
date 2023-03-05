e3182748d5118025f68d90dd4853a79e7e9a5a94
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-03-05 17:05:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let percentage = document.querySelector(".percentage");
+let percent = document.querySelector(".percent");
 
+navigator.getBattery().then(function (battery) {
+  percentage.style.width = battery.level * 100 + "%";
+  percent.innerHTML = Math.floor(battery.level * 100) + "%";
+});
+
+let sec = document.querySelector(".sec");
+let toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", function () {
+  sec.classList.toggle("dark");
+});
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Pri
